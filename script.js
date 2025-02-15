fb6994c4c66e503102cb7d87e2ce568df541e613
matejabodanovic <bogdanovicmateja75@gmail.com>
2025-02-15 23:00:33 +0100

    Estimated time.

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ let hamburger = document.getElementById("hamburger");
 hamburger.addEventListener("change", function () {
   if (!this.checked) {
     document.location.href = "#navigation";
-  } else {
   }
 });
