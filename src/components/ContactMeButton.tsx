33b6861c559a5a621d28768a3060afb1444dd867
matejabodanovic <bogdanovicmateja75@gmail.com>
2025-04-15 11:44:38 +0200

    Contact.

--- src/components/ContactMeButton.tsx
+++ src/components/ContactMeButton.tsx
@@ -5,7 +5,7 @@ const ContactMeButton = () => {
   const context: OutletContextType = useOutletContext();
   return (
     <Link
-      to="#"
+      to="mailto:bogdanovicmateja75@gmail.com"
       className="block cursor-pointer uppercase font-bold text-accent border-2 xl:border-transparent rounded-full xl:hover:border-accent border-accent active-link py-7 px-10"
     >
       {context.language === 0 ? "Contact Me" : "Kontakt"}
